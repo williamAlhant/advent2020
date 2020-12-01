402a8efcdb0f6762b3f804871434597f809a6354
william <wi.alhant@gmail.com>
2020-12-01 19:59:32 +0100

    Day 1 done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    assert!(args.len() >= 2);
+    let input_path = args.nth(1).unwrap();
+    let file = File::open(input_path).unwrap_or_else(exit_with_error_message);
+    let lines = BufReader::new(file).lines()
+        .map(|l| l.unwrap_or_else(exit_with_error_message))
+        .map(|l| l.parse::<i32>().unwrap_or_else(exit_with_error_message));
+    let nums: Vec<i32> = lines.collect();
+
+    for i in 0..nums.len() {
+        for j in i..nums.len() {
+            for k in j..nums.len() {
+                if nums[i] + nums[j] + nums[k] == 2020 {
+                    println!("Answer: {}", nums[i] * nums[j] * nums[k]);
+                    return;
+                }
+            }
+        }
+    }
+}
+
+fn exit_with_error_message<ROk, RErr>(e: RErr) -> ROk
+    where RErr: std::fmt::Display {
+    println!("{}", e);
+    process::exit(-1);
 }
