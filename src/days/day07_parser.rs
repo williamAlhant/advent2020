e18d168c853af6eb516df224610eaea640b467b6
william <wi.alhant@gmail.com>
2020-12-07 19:55:32 +0100

    wip

--- src/days/day07_parser.rs
+++ src/days/day07_parser.rs
@@ -1,8 +1,8 @@
-use super::day07_struct::{Rule, BagTypeRegist};
+use super::day07_struct::{Rule, NumBags, BagTypeRegist};
 use anyhow::{Result, bail};
 
-pub fn parse_rule(i: &str) -> Result<Rule> {
-    bail!("");
+pub fn parse_rule(i: &str, regist: &mut BagTypeRegist) -> Result<Rule> {
+    
 }
 
 #[cfg(test)]
@@ -11,9 +11,9 @@ mod test {
 
     #[test]
     fn parse() {
-        let regist = BagTypeRegist::default();
+        let mut regist = BagTypeRegist::default();
         let i = "light red bags contain 1 bright white bag, 2 muted yellow bags.";
-        let o = parse_rule(i).unwrap();
-        assert!(o.to_str(&regist) == i);
+        let o = parse_rule(i, &mut regist).unwrap();
+        assert_eq!(o.to_str(&regist), i);
     }
 }
--- src/days/day07_struct.rs
+++ src/days/day07_struct.rs
@@ -31,7 +31,7 @@ fn elements_to_str(elements: &Vec<NumBags>, regist: &BagTypeRegist) -> String {
             }
         ));
 
-        if i != elements.len() {
+        if i < elements.len() - 1 {
             s.push_str(", ");
         }
     }
@@ -44,3 +44,24 @@ impl Rule {
             regist.index_to_str[self.bag_type], elements_to_str(&self.elements, regist))
     }
 }
+
+impl BagTypeRegist {
+    pub fn add(&mut self, bag_type: String) {
+        self.index_to_str.push(bag_type.clone());
+        self.str_to_index.insert(bag_type, self.index_to_str.len() - 1);
+    }
+
+    pub fn get_type(&self, s: &str) -> Option<BagType> {
+        self.str_to_index.get(s).map(|x| x.clone())
+    }
+}
+
+// impl BagTypeRegist {
+//     pub fn test() -> Self {
+//         let mut regist = BagTypeRegist::default();
+//         regist.add("light red".to_string());
+//         regist.add("bright white".to_string());
+//         regist.add("muted yellow".to_string());
+//         regist
+//     }
+// }
