485403a2215e3366dd5187ac2bd5a7407a107fa2
william <wi.alhant@gmail.com>
2020-12-08 00:24:22 +0100

    Not sure why that works

--- src/days/day07_parser.rs
+++ src/days/day07_parser.rs
@@ -32,33 +32,41 @@ fn parse_elements<'a>(
 
     move |i: &str| {
         let (i_remain, _) = many1_count(
-            |j: &str| {
-
-                let mut rule_el = NumBags::default();
-
-                let (remain, digits) = digit1(j)?;
-                rule_el.num = digits.parse().unwrap();
-
-                let (remain, bag_type) = preceded(
-                    tag(" "), 
-                    take_until(" bag")
-                )(remain)?;
-                rule_el.bag_type = regist.get_or_else_add(bag_type);
-
-                let (remain, _) = preceded(
-                    tag(" bag"),
-                    preceded(
-                        opt(tag("s")),
-                        opt(tag(", "))
-                    )
-                )(remain)?;
-
-                rule.elements.push(rule_el);
-
-                Ok((remain, ()))
-            }
+            parse_element(rule, regist)
         )(i)?;
         Ok((i_remain,()))
+    }
+}
+
+fn parse_element<'a>(
+    rule: &'a mut Rule,
+    regist: &'a mut BagTypeRegist
+) -> impl FnMut(&str) -> IResult<&str, ()> + 'a {
+
+    move |j: &str| {
+
+        let mut rule_el = NumBags::default();
+
+        let (remain, digits) = digit1(j)?;
+        rule_el.num = digits.parse().unwrap();
+
+        let (remain, bag_type) = preceded(
+            tag(" "), 
+            take_until(" bag")
+        )(remain)?;
+        rule_el.bag_type = regist.get_or_else_add(bag_type);
+
+        let (remain, _) = preceded(
+            tag(" bag"),
+            preceded(
+                opt(tag("s")),
+                opt(tag(", "))
+            )
+        )(remain)?;
+
+        rule.elements.push(rule_el);
+
+        Ok((remain, ()))
     }
 }
 
